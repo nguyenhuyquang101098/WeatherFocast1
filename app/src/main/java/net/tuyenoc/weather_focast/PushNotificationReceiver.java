790298bb4aac42fe7af7590d3303fde32a5f7141
tuyennguyen05071998 <tuyennguyen05071998@gmail.com>
2020-02-24 21:44:19 +0700

    commit test

--- app/src/main/java/net/tuyenoc/weather_focast/PushNotificationReceiver.java
+++ app/src/main/java/net/tuyenoc/weather_focast/PushNotificationReceiver.java
@@ -2,4 +2,5 @@ package net.tuyenoc.weather_focast;
 
 public class PushNotificationReceiver {
     private static String TAG = "PushNotificationReceiver";
+    private static String A = "PushNotificationReceiver";
 }
